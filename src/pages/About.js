0d354e1b0f26a89272f42b32a6cc5fe090e164b7
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-06-20 02:24:07 +0200

    First Commit

---
+++ src/pages/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1> A propos </h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        suscipit iusto, facere eaque exercitationem dolor labore impedit
+        inventore minus, necessitatibus debitis deleniti veritatis fugiat
+        soluta. Excepturi voluptatum ea cupiditate beatae.
+      </p>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+        blanditiis esse. Esse quibusdam laborum ea dolore sit iure tempora quasi
+        neque quos voluptate, repellendus quae sapiente doloremque
+        reprehenderit, error nesciunt quod necessitatibus sed modi explicabo nam
+        cupiditate cumque. Possimus asperiores soluta saepe consectetur hic
+        provident pariatur magni suscipit sapiente, amet reprehenderit a
+        blanditiis dolorem earum quis cumque quod corrupti quo architecto
+        perferendis voluptatibus expedita? Veritatis sint explicabo vitae ea. Id
+        adipisci voluptatum cumque animi aut, odit maiores aperiam magnam libero
+        itaque! Dicta vero, cupiditate praesentium quisquam ipsa optio quae
+        nulla ullam voluptate animi illo rem voluptatibus maiores repellat
+        fugiat quidem.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Countries from "../component/Countries";
+
+const Home = () => {
+  return (
+    <div>
+      <Countries />
+    </div>
+  );
+};
+
+export default Home;
