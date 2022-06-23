1036b20a2eb3ad0df703a54628352c642052f8a1
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-06-24 01:24:38 +0200

    button select

--- src/component/Countries.js
+++ src/component/Countries.js
@@ -29,16 +29,24 @@ const Countries = () => {
               type="radio"
               id={continent}
               name="continentName"
+              checked={continent === selectedRadio}
               onChange={(e) => setSelectedRadio(e.target.id)}
             />
             <label htmlFor={continent}>{continent}</label>
           </li>
         ))}
       </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          {" "}
+          Annuler la recherche{" "}
+        </button>
+      )}
 
       <ul>
         {data
           .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
           .slice(0, rangeValue)
           .map((country, index) => (
             <Card key={index} country={country} />
